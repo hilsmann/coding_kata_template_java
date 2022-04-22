daac55da3bc7eab381dca7380001458c8a018b74
Sven Hilsmann <sven.hilsmann.ext@uniper.energy>
2022-04-22 13:03:07 +0200

    Create Coding Kata template

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DummyTest {
+
+    @Test
+    void dummy() {
+        assertEquals(1, 1);
+    }
+}
